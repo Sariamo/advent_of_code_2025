f26726b6a4f149e239c42879dd873b36dccbbd49
Lennard Wiecking <lennard.wiecking@gmx.de>
2025-12-14 22:25:41 +0100

    Add day eight

---
+++ src/8/a.py
@@ -0,0 +1,73 @@
+import sys
+
+
+def d(coord1, coord2):
+    x1, y1, z1 = coord1
+    x2, y2, z2 = coord2
+    return abs(int(x2) - int(x1)) ** 2 + abs(int(y2) - int(y1)) ** 2 + abs(int(z2) - int(z1)) ** 2
+
+coords = []
+with open(sys.argv[1]) as inp:
+    for row in inp:
+        if row != "" and row != "\n":
+            coords.append(row.replace("\n", "").split(","))
+
+connection_ctr = 0
+connections = []
+connections_plat = []
+distances = []
+
+for coord1 in coords:
+    for coord2 in coords:
+        if coord1 == coord2 or [coord2, coord1] in connections_plat:
+            continue
+        distances.append(d(coord1, coord2))
+        connections_plat.append([coord1, coord2])
+        connections_plat = [x for _, x in sorted(zip(distances, connections_plat))][:1000]
+        distances = sorted(distances)[:1000]
+
+for shortest_coords in connections_plat:
+    new_con_1, new_con_2 = shortest_coords
+    in_c = False
+    for c in connections:
+        for con_mem in c:
+            if con_mem == new_con_1 and new_con_2 in c or con_mem == new_con_2 and new_con_1 in c:
+                in_c = True
+                break
+            if con_mem == new_con_1 and new_con_2 not in c:
+                c.append(new_con_2)
+                in_c = True
+                break
+            if con_mem == new_con_2 and new_con_1 not in c:
+                c.append(new_con_1)
+                in_c = True
+                break
+        if in_c:
+            break
+
+    if not in_c:
+        connections.append(shortest_coords)
+
+    b = False
+    for c1 in connections:
+        for c2 in connections:
+            for con_mem1 in c1:
+                if c1 != c2 and con_mem1 in c2:
+                    new_c = c1
+                    new_c.extend(c2)
+                    new_c_unique = []
+                    for nc in new_c:
+                        if nc not in new_c_unique:
+                            new_c_unique.append(nc)
+                    connections.append(new_c_unique)
+                    connections.remove(c1)
+                    connections.remove(c2)
+                    b = True
+                    break
+            if b:
+                break
+        if b:
+            break
+
+lengths = sorted([len(c) for c in connections], reverse=True)
+print(lengths[0] * lengths[1] * lengths[2])
